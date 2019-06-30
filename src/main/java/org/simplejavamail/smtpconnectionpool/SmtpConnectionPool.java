722635ef14839bef4ff9ff57b7ec8d3ac2bfde9d
bbottema <b.bottema@projectnibble.org>
2019-06-30 15:24:53 +0200

    unused annotation

--- src/main/java/org/simplejavamail/smtpconnectionpool/SmtpConnectionPool.java
+++ src/main/java/org/simplejavamail/smtpconnectionpool/SmtpConnectionPool.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import javax.mail.Session;
 import javax.mail.Transport;
 
-@SuppressWarnings("WeakerAccess")
 public class SmtpConnectionPool extends ResourceClusters<Session, Session, Transport> {
     public SmtpConnectionPool(@NotNull final SmtpClusterConfig smtpClusterConfig) {
         super(smtpClusterConfig.getConfigBuilder().build());
